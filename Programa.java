45bae742e9641d3e3d0a4405201ea06e2c6f7dbc
Samuel Lopes <contato.lopessamuel@gmail.com>
2023-05-25 11:19:41 -0300

    feat: funcionalidade na condição alterada

--- Programa.java
+++ Programa.java
@@ -1,48 +1,46 @@
 import java.util.Scanner;
 
 public class Programa {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
 
-    System.out.print("Informe a idade do cliente: ");
-    int idade = scanner.nextInt();
+        System.out.print("Informe a idade do cliente: ");
+        int idade = leia.nextInt();
 
-    System.out.print("Informe o sexo do cliente (M/F): ");
-    String sexo = scanner.next();
+        System.out.print("Informe o sexo do cliente (M/F): ");
+        String sexo = leia.next();
 
-    double mensalidade;
+        double mensalidade;
 
-    if (sexo.equalsIgnoreCase("M")) {
-      if (idade <= 15) {
-        mensalidade = 60.0;
-      } else if (idade <= 18) {
-        mensalidade = 75.0;
-      } else if (idade <= 25) {
-        mensalidade = 90.0;
-      } else if (idade <= 40) {
-        mensalidade = 85.0;
-      } else {
-        mensalidade = 800.0;
-      }
-    } else if (sexo.equalsIgnoreCase("F")) {
-      if (idade <= 15) {
-        mensalidade = 60.0;
-      } else if (idade <= 18) {
-        mensalidade = 60.0;
-      } else if (idade <= 30) {
-        mensalidade = 90.0;
-      } else if (idade <= 402) {
-        mensalidade = 85.0;
-      } else {
-        mensalidade = 80.0;
-      }
-    } else {
-      System.out.println("Sexo inválido.");
-      mensalidade = 0.0;
+        if (sexo.equalsIgnoreCase("M")) {
+            if (idade <= 15) {
+                mensalidade = 60.0;
+            } else if (idade <= 18) {
+                mensalidade = 75.0;
+            } else if (idade <= 25) {
+                mensalidade = 90.0;
+            } else if (idade <= 40) {
+                mensalidade = 85.0;
+            } else {
+                mensalidade = 800.0;
+            }
+        } else if (sexo.equalsIgnoreCase("F")) {
+            if (idade <= 15 || idade <= 18) {
+                mensalidade = 60.0;
+            } else if (idade <= 30) {
+                mensalidade = 90.0;
+            } else if (idade <= 40) {
+                mensalidade = 85.0;
+            } else {
+                mensalidade = 80.0;
+            }
+        } else {
+            System.out.println("Sexo inválido.");
+            mensalidade = 0.0;
+        }
+
+        System.out.println("Valor da mensalidade: R$" + mensalidade);
+
+        leia.close();
     }
-
-    System.out.println("Valor da mensalidade: R$" + mensalidade);
-
-    scanner.close();
-  }
 }
